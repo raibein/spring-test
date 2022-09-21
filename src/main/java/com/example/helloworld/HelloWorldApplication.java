8b68371d92b1ab04bd586f758d60a4f11286b6e8
Shrestha <r.shrestha@sap.com>
2022-09-22 00:44:58 +0200

    update code for deploy war in tomcat

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -2,6 +2,7 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -9,6 +10,11 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HelloWorldApplication.class);
     }
 
 }
